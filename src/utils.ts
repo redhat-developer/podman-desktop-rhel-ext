7ad0e607ab86a8da76044ab6fe6482adadd4f4cc
Philippe Martin <phmartin@redhat.com>
2025-04-30 14:24:52 +0200

    chore: remove dead code

--- src/utils.ts
+++ src/utils.ts
@@ -16,55 +16,6 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import { existsSync } from 'node:fs';
-import { homedir } from 'node:os';
-import { join, resolve } from 'node:path';
-
-const localBinDir = '/usr/local/bin';
-
-/**
- * Calculate the system path of the binary
- * @returns
- */
-export function getSystemBinaryPath(binaryName: string): string {
-  switch (process.platform) {
-    case 'win32':
-      return join(
-        homedir(),
-        'AppData',
-        'Local',
-        'Microsoft',
-        'WindowsApps',
-        binaryName.endsWith('.exe') ? binaryName : `${binaryName}.exe`,
-      );
-    case 'darwin':
-    case 'linux':
-      return join(localBinDir, binaryName);
-    default:
-      throw new Error(`unsupported platform: ${process.platform}.`);
-  }
-}
-
-/**
- * Given an executable name, it will find where it is installed on the system.
- * It first try to search it in the system-wide folder, then in the extension storage
- * @param executable
- */
-export async function whereBinary(storagePath: string, binaryName: string): Promise<string> {
-  const macadamSystemWidePath = getSystemBinaryPath(binaryName);
-  if (existsSync(macadamSystemWidePath)) {
-    return macadamSystemWidePath;
-  }
-
-  const macadamStoragePath = resolve(storagePath, 'bin', binaryName);
-  if (existsSync(macadamStoragePath)) {
-    return macadamStoragePath;
-  }
-
-  // if it's not installed either in the extension storage path or system wide throw an error
-  throw new Error('no macadam binary found');
-}
-
 export function getErrorMessage(err: unknown): string {
   if (err && typeof err === 'object' && 'message' in err) {
     return String(err.message);
