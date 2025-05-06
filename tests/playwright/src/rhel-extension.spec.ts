25454096c345fa0830cb63965b9bf369e8e803d8
Vladimir Lazar <106525396+cbr7@users.noreply.github.com>
2025-05-06 10:11:27 +0200

    chore(test): configure retry value (#95)
    Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/rhel-extension.spec.ts
+++ tests/playwright/src/rhel-extension.spec.ts
@@ -29,8 +29,7 @@ const extensionLabel = 'redhat.macadam';
 const extensionHeading = 'Macadam';
 let extensionInstalled = false;
 const skipInstallation = process.env.SKIP_INSTALLATION;
-const extensionURL =
-  process.env.OCI_IMAGE ?? 'ghcr.io/redhat-developer/podman-desktop-rhel-ext:next';
+const extensionURL = process.env.OCI_IMAGE ?? 'ghcr.io/redhat-developer/podman-desktop-rhel-ext:next';
 
 test.use({
   runnerOptions: new RunnerOptions({
@@ -52,12 +51,13 @@ test.afterAll(async ({ runner }) => {
 });
 
 test.describe.serial('RHEL Extension E2E Tests', () => {
+  test.describe.configure({ retries: 1 });
   test('Go to settings and check if extension is already installed', async ({ navigationBar }) => {
     const extensionsPage = await navigationBar.openExtensions();
     if (await extensionsPage.extensionIsInstalled(extensionLabel)) extensionInstalled = true;
   });
 
-  test('Uninstalled previous version of bootc extension', async ({ navigationBar }) => {
+  test('Uninstalled previous version of rhel extension', async ({ navigationBar }) => {
     test.skip(!extensionInstalled || !!skipInstallation);
     test.setTimeout(120_000);
     console.log('Extension found already installed, trying to remove!');
