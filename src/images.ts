a2fda374ad4f89910fce107278c3b202d0ef86f4
Philippe Martin <phmartin@redhat.com>
2025-05-06 10:11:43 +0200

    fix: rename id

--- src/images.ts
+++ src/images.ts
@@ -21,13 +21,13 @@ import { arch } from 'node:os';
 const IMAGES: { [provider: string]: string } = {
   applehv: '24f35ffb80911f2687f0bcd4237f62e46c19a6f0445aacb951b77d1974130187',
   wsl: '1351d19fddb169ed01dc8815e9318027d27d7fe8c80e1844559ccd9c041ad9ca',
-  nativex64: '9d11248599b91178a600202412ad3ffc6f1c75c050d7b3c5484dc3f46fc06582',
+  linux_native_x64: '9d11248599b91178a600202412ad3ffc6f1c75c050d7b3c5484dc3f46fc06582',
 };
 
 export function getImageSha(provider?: string): string {
   if (!provider) {
     if (arch() === 'x64') {
-      return IMAGES['nativex64'];
+      return IMAGES['linux_native_x64'];
     } else {
       throw new Error('linux non-x64 is not supported');
     }
