3e5509bdaf0cec485aa2271d2e2cbb0d42beea49
Vladimir Lazar <vlazar@redhat.com>
2025-09-12 12:56:49 +0300

    chore(Test): skip in cicd

--- tests/playwright/src/rhel-extension.spec.ts
+++ tests/playwright/src/rhel-extension.spec.ts
@@ -107,6 +107,8 @@ test.describe.serial('RHEL Extension E2E Tests', () => {
   });
 
   test.describe.serial('Red Hat Authentication extension installation', () => {
+    test.skip(!!isCI && !!isLinux, 'Skipping on CI GitHub Actions for Linux runners, they are not supported');
+
     let chromiumPage: Page | undefined;
     let browser: Browser | undefined;
     let context: BrowserContext | undefined;
