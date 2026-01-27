50c40293d851967a3758a1b08b8fb474f53039e0
Florent Benoit <fbenoit@redhat.com>
2026-01-27 14:59:34 +0100

    feat: add images for linux/arm64
    fixes https://github.com/redhat-developer/podman-desktop-rhel-ext/issues/415

--- src/images.ts
+++ src/images.ts
@@ -29,6 +29,10 @@ const IMAGES: { [provider: string]: { [version: string]: string } } = {
     [RHEL_VMS_IMAGE_PROPERTY_VALUE_RHEL_10]: 'e1871004d0075e0ce10cfb4c1aae7c1fb56cf2162e9d494d1f9c9d061902fec6', // 10.1
     [RHEL_VMS_IMAGE_PROPERTY_VALUE_RHEL_9]: 'bf4fa1142e0090e7f6fe163bf03d5f0de12ebafce02d4f8ea71dd5de3f1769c8', // 9.7
   },
+  linux_native_arm64: {
+    [RHEL_VMS_IMAGE_PROPERTY_VALUE_RHEL_10]: 'a522f6abacab1c5804477332bbd14a467c3f9812d3f2c46ee05c71b07df05bcf', // 10.1
+    [RHEL_VMS_IMAGE_PROPERTY_VALUE_RHEL_9]: 'e424cba737c5d6315160111043f40108b3bc458c09c937454a3331c52503d877', // 9.7
+  },
   linux_native_x64: {
     [RHEL_VMS_IMAGE_PROPERTY_VALUE_RHEL_10]: 'dc74ad1a9ccd3c62a02cc29f7f4715e47fa0fdaf08a8080dd20906f885f29bae', // 10.1
     [RHEL_VMS_IMAGE_PROPERTY_VALUE_RHEL_9]: 'e89e0a3e28c3e7fb73a97483b60130d5a3e4bba9481ae282ec6e0551ccf30047', // 9.7
@@ -39,8 +43,10 @@ export function getImageSha(provider: string | undefined, version: string): stri
   if (!provider) {
     if (arch() === 'x64') {
       return IMAGES['linux_native_x64'][version];
+    } else if (arch() === 'arm64') {
+      return IMAGES['linux_native_arm64'][version];
     } else {
-      throw new Error('linux non-x64 is not supported');
+      throw new Error('linux non-x64/arm64 is not supported');
     }
   }
   if (provider in IMAGES) {
